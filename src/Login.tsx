9bddc45541ec85486b5e3b24fa2fa2b57d1c393e
oeportil <ernestoportilllo09@gmail.com>
2024-09-10 23:33:24 -0600

    Login y Registro

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,7 +1,34 @@
+import { Link } from 'react-router-dom'
+import logo from './img/logo.png'
+import { Button, Card, Form } from "react-bootstrap"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='d-flex justify-content-center align-items-center mt-5 mx-4'>
+        <Card style={{ width: '30rem' }}>
+          <Card.Img variant="top" className='w-50 mx-auto' src={logo} alt="Cheflim Logo"/>
+          <Card.Body>
+            <Card.Title className="text-uppercase text-center text-danger fw-bolder">Cheflim</Card.Title>
+            <Card.Text>
+              <Form>
+                <Form.Group className="mb-3" controlId="email.ControlInput1">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="password.ControlInput">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder='...' />
+                </Form.Group>
+                <Button className='bg-red bg-red-hover border-0 rounded-5 w-100'>Iniciar Sesión</Button>
+              </Form>
+              <hr />
+              <div className='d-flex justify-content-center'>
+                <Link to={"/register"} className='text-green fw-semibold'>Crear Cuenta</Link>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+    </div>
   )
 }
 
--- src/Register.tsx
+++ src/Register.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <div className="bg-red d-flex align-items-center p-4 text-white ">
+          <Link to={"/login"} > {"<"} </Link>
+          <h2 className="">Crea tu cuenta</h2>
+      </div>
+    </>
   )
 }
 
