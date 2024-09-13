2339c12e9db76936acc82daf7c30bf06b95d68ba
BryanVas24 <bryanvasquez2406@gmail.com>
2024-09-13 17:39:24 -0600

    Iniciando Registro

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,41 +1,134 @@
-import { Button, Form } from "react-bootstrap"
-import { Link } from "react-router-dom"
-import './styles/registerStyles.css';
+import { Button, Form, InputGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaEnvelope, FaUser, FaLock, FaEye, FaEyeSlash } from "react-icons/fa"; // Importa los íconos necesarios
+import { useState } from "react";
+import "./styles/registerStyles.css";
 
 const Register = () => {
+  //states para ver las passwords
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState<boolean>(false);
+
+  //función de registro
+  const registerAccount = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const userData = {
+      email: formData.get("email"),
+      nombreCompleto: formData.get("nombreCompleto"),
+      nombreUsuario: formData.get("nombreUsuario"),
+      password: formData.get("password"),
+    };
+
+    const repetirPassword = formData.get("repetirPassword");
+
+    if (repetirPassword != userData.password) {
+      return;
+    }
+
+    const emptyFields = Object.values(userData);
+    if (emptyFields.includes(" ")) {
+      console.log("cojone falta un campo");
+      return;
+    }
+  };
+
   return (
     <>
-      <div className="bg-red d-flex  gap-3 p-4 text-white ">
-          <Link to={"/login"} className="text-white fs-4 fw-bolder"> {"<"} </Link>
-          <h2 className="">Crea tu cuenta</h2>
+      <div className="bg-red d-flex gap-3 p-4 text-white">
+        <Link to={"/login"} className="text-white fs-4 fw-bolder">
+          {" "}
+          {"<"}{" "}
+        </Link>
+        <h2 className="">Crea tu cuenta</h2>
       </div>
       <div className="d-flex justify-content-center mt-3">
-        <Form className="register-max-w">
-          <Form.Group className="mb-3" controlId="email.ControlInput1">
+        <Form className="register-max-w" onSubmit={registerAccount}>
+          <Form.Group className="mb-3">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+            <InputGroup>
+              <InputGroup.Text>
+                <FaEnvelope />
+              </InputGroup.Text>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                name="email"
+              />
+            </InputGroup>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="nombre.ControlInput1">
+          <Form.Group className="mb-3">
             <Form.Label>Nombre Completo</Form.Label>
-            <Form.Control type="text" placeholder="ejemplo" />
+            <InputGroup>
+              <InputGroup.Text>
+                <FaUser />
+              </InputGroup.Text>
+              <Form.Control
+                type="text"
+                placeholder="ejemplo"
+                name="nombreCompleto"
+              />
+            </InputGroup>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="nombre.ControlInput1">
+          <Form.Group className="mb-3">
             <Form.Label>Nombre de Usuario</Form.Label>
-            <Form.Control type="text" placeholder="Cheflim34" />
+            <InputGroup>
+              <InputGroup.Text>
+                <FaUser />
+              </InputGroup.Text>
+              <Form.Control
+                type="text"
+                placeholder="Cheflim34"
+                name="nombreUsuario"
+              />
+            </InputGroup>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="password.ControlInput">
+          <Form.Group className="mb-3">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder='...' />
+            <InputGroup>
+              <InputGroup.Text>
+                <FaLock />
+              </InputGroup.Text>
+              <Form.Control
+                type={showPassword ? "text" : "password"}
+                placeholder={showPassword ? "Password" : "*****"}
+                name="password"
+              />
+              <InputGroup.Text onClick={() => setShowPassword(!showPassword)}>
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </InputGroup.Text>
+            </InputGroup>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="password.ControlInput">
+          <Form.Group className="mb-3">
             <Form.Label>Repetir Password</Form.Label>
-            <Form.Control type="password" placeholder='...' />
+            <InputGroup>
+              <InputGroup.Text>
+                <FaLock />
+              </InputGroup.Text>
+              <Form.Control
+                type={showRepeatPassword ? "text" : "password"}
+                placeholder={showRepeatPassword ? "Repetir password" : "*****"}
+                name="repetirPassword"
+              />
+              <InputGroup.Text
+                onClick={() => setShowRepeatPassword(!showRepeatPassword)}
+              >
+                {showRepeatPassword ? <FaEyeSlash /> : <FaEye />}
+              </InputGroup.Text>
+            </InputGroup>
           </Form.Group>
-          <Button className='bg-red bg-red-hover border-0 rounded-5 w-100'>Iniciar Sesión</Button>
+          <Button
+            type="submit"
+            className="bg-red bg-red-hover border-0 rounded-5 w-100"
+          >
+            Iniciar Sesión
+          </Button>
         </Form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
